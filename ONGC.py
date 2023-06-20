975dec6be76079c2f0e618bd5051f6f0b4a302dc
Dhruv <pa8618@srmist.edu.in>
2023-06-20 16:24:36 +0530

    Plotting initialized

--- ONGC.py
+++ ONGC.py
@@ -1,17 +1,25 @@
 import math
+import matplotlib.pyplot as plt
+
 
 def calculate_phase_angle(time_stamp, amplitude):
     time_radians = (time_stamp / 24) * 2 * math.pi
     phase_angle = math.atan(amplitude)
     phase_angle_deg = math.degrees(phase_angle)
 
-    return phase_angle_deg
+    return round(phase_angle_deg,2)
 time_stamps = [0, 2, 4, 6, 8]  
 amplitudes = [10, 20, -20, -30, 30]  
-
+phase_angles = []
 
 for i in range(len(time_stamps)):
     time_stamp = time_stamps[i]
     amplitude = amplitudes[i]
     phase_angle = calculate_phase_angle(time_stamp, amplitude)
-    print(f"Phase angle at time stamp {time_stamp} millisecond: {round(phase_angle,3)} degrees")
+    phase_angles.append(phase_angle)
+   # print(f"Phase angle at time stamp {time_stamp} millisecond: {phase_angle} degrees")
+print(phase_angles)
+plt.plot(time_stamps, phase_angles, 'o')
+plt.xlabel('Time-stamps')
+plt.ylabel('Phase_angles')
+plt.show()
