92fdee49d24f7899a5cb8cbadc1e0f47917b9a82
Dhruv <pa8618@srmist.edu.in>
2023-06-21 10:43:11 +0530

    updates

--- ONGC.py
+++ ONGC.py
@@ -6,8 +6,8 @@ def calculate_phase_angle(time_stamp, amplitude):
     time_radians = (time_stamp / 24) * 2 * math.pi
     phase_angle = math.atan(amplitude)
     phase_angle_deg = math.degrees(phase_angle)
-
     return round(phase_angle_deg,2)
+
 time_stamps = [0, 2, 4, 6, 8]  
 amplitudes = [10, 20, -20, -30, 30]  
 phase_angles = []
