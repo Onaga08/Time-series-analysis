6a7564c9ac4b72bc524633defec641e373612a44
Dhruv <pa8618@srmist.edu.in>
2023-06-22 10:44:46 +0530

    Phase lag code

--- lag.py
+++ lag.py
@@ -1,31 +1,34 @@
-import math
 import numpy as np
-import matplotlib.pyplot as plt
+
+Amplitudes1 = [-0.393105, -0.822565, -1.32783, -1.84869, -2.30533, -2.52846, -2.40997, -1.97941, -1.24355, -0.517542, 0.0990675, 1.13414, 2.17622, 3.01957, 3.55358, 3.72256, 3.47835, 2.7966, 1.96388, 1.23775,0.51741]
+Amplitudes2 = [0.865257, 2.09646, 3.55949, 5.0869, 6.39701, 7.34355, 7.71642, 7.22113, 5.87831, 3.78083, 1.15362, -1.68, -4.35489, -6.53074, -7.95374, -8.48222, -8.08787, -7.15307, -5.63978, -3.91215,-2.22282]
+Amplitudes_comp = [-0.0413589, -0.0744249, -0.108228, -0.161074, -0.224722, -0.291076, -0.334128, -0.333282, -0.271283, -0.153474, -0.00260863, 0.153474, 0.271283, 0.333282, 0.334128, 0.291076, 0.224722, 0.161074, 0.108228, 0.0744249]
+
+sample_rate = 2
+
+maxima1_indices1 = np.where(np.diff(np.sign(np.diff(Amplitudes1))) < 0)[0] + 1
+maxima2_indices1 = np.where(np.diff(np.sign(np.diff(Amplitudes_comp))) < 0)[0] + 1
+
+minima1_indices1 = np.where(np.diff(np.sign(np.diff(Amplitudes1))) > 0)[0] + 1
+minima2_indices1 = np.where(np.diff(np.sign(np.diff(Amplitudes_comp))) > 0)[0] + 1
+
+phase_lag_crest_to_crest1 = (maxima2_indices1[0] - maxima1_indices1[0]) * sample_rate
+
+phase_lag_trough_to_trough1 = (minima2_indices1[0] - minima1_indices1[0]) * sample_rate
+
+print("Phase Lag (Crest to Crest)1-3:", phase_lag_crest_to_crest1, "seconds")
+print("Phase Lag (Trough to Trough)1-3:", phase_lag_trough_to_trough1, "seconds")
 
 
-graph1 = np.array([0.865257, 2.09646, 3.55949, 5.0869, 6.39701, 7.34355, 7.71642, 7.22113, 5.87831, 3.78083, 1.15362, -1.68, -4.35489, -6.53074, -7.95374, -8.48222, -8.08787, -7.15307, -5.63978, -3.91215])
+maxima1_indices1 = np.where(np.diff(np.sign(np.diff(Amplitudes2))) < 0)[0] + 1
+maxima2_indices1 = np.where(np.diff(np.sign(np.diff(Amplitudes_comp))) < 0)[0] + 1
 
+minima1_indices1 = np.where(np.diff(np.sign(np.diff(Amplitudes2))) > 0)[0] + 1
+minima2_indices1 = np.where(np.diff(np.sign(np.diff(Amplitudes_comp))) > 0)[0] + 1
 
-graph2 = np.array([-0.0413589, -0.0744249, -0.108228, -0.161074, -0.224722, -0.291076, -0.334128, -0.333282, -0.271283, -0.153474, -00000000.260863, 0.153474, 0.271283, 0.333282, 0.334128, 0.291076, 0.224722, 0.161074, 0.108228, 0.0744249])
+phase_lag_crest_to_crest1 = (maxima2_indices1[0] - maxima1_indices1[0]) * sample_rate
 
-cross_corr = np.correlate(graph1, graph2, 'full')
+phase_lag_trough_to_trough1 = (minima2_indices1[0] - minima1_indices1[0]) * sample_rate
 
-
-max_index = np.argmax(np.abs(cross_corr))
-
-phase_lag_rad = (max_index - len(graph1) + 1) * (2 * math.pi / len(graph1))
-
-
-phase_lag_deg = math.degrees(phase_lag_rad)
-
-print("Phase Lag (radians):", phase_lag_rad)
-print("Phase Lag (degrees):", phase_lag_deg)
-
-
-shifts = np.arange(len(graph1) - 1, len(graph2))
-plt.plot(shifts, cross_corr)
-plt.xlabel('Shifts')
-plt.ylabel('Cross-correlation')
-plt.title('Cross-correlation between Graph 1 and Graph 2')
-plt.grid(True)
-plt.show()
+print("Phase Lag (Crest to Crest)2-3:", phase_lag_crest_to_crest1, "seconds")
+print("Phase Lag (Trough to Trough)2-3:", phase_lag_trough_to_trough1, "seconds")
